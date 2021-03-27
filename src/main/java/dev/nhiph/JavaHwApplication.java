a6b138492584b5858e0fd2342c023fd57b604915
nhiph <phunghuynhnhi797@gmail.com>
2021-03-27 15:57:57 +0700

    first commit

---
+++ src/main/java/dev/nhiph/JavaHwApplication.java
@@ -0,0 +1,13 @@
+package dev.nhiph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaHwApplication.class, args);
+    }
+
+}
